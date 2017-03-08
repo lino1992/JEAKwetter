7a3c1d879c75ff4329290c8451c059acd65be6ae
Thaencharun,lino S <l.thaencharun@student.fontys.nl>
2017-03-08 20:56:04 +0100

    Remove test persistence file

--- JEAKwetter/target/generated-sources/annotations/domain/Role_.java
+++ JEAKwetter/target/generated-sources/annotations/domain/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T20:26:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T20:38:59")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
